ffb16081e40c6f00b1aa4044833bc8a8e5020b02
Natalija Kitanoska <n.kitanoska@dyntechdoo.com>
2016-06-07 14:47:25 +0200

    no message

--- src/main/java/com/ShoppingCart/entity/Product.java
+++ src/main/java/com/ShoppingCart/entity/Product.java
@@ -1,14 +1,11 @@
 package com.ShoppingCart.entity;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -20,6 +17,9 @@ public class Product {
 	private Integer id;
 	
 	@Column(name="name")
+	private String name;
+		
+	@Column(name="description")
 	private String description;
 	
 	@Column(name="price")
