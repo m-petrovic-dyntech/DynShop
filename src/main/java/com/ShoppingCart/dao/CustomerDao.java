89bd59ae5c7c596ed850f7c83ee8d690b22b88ea
j-milosavljevic <j.milosavljevic@dyntechdoo.com>
2016-06-14 14:44:18 +0200

    Backend- CustomerDao and CustomerDaoImpl
    added get. set, edit etc. methods

--- src/main/java/com/ShoppingCart/dao/CustomerDao.java
+++ src/main/java/com/ShoppingCart/dao/CustomerDao.java
@@ -1,5 +1,20 @@
 package com.ShoppingCart.dao;
 
+import java.util.ArrayList;
+
+import com.ShoppingCart.entity.Customer;
+
 public interface CustomerDao {
+
+	public ArrayList<Customer> getAllCustomers();
+
+	public Customer getCustomer(int id);
+	
+	public void addCustomer(Customer customer);
+	
+	public void editCustomer(int id, Customer newCustomer);
+	
+	public void deleteCustomer(int id);
+	
 
 }
--- src/main/java/com/ShoppingCart/dao/impl/CustomerDaoImpl.java
+++ src/main/java/com/ShoppingCart/dao/impl/CustomerDaoImpl.java
@@ -1,7 +1,74 @@
 package com.ShoppingCart.dao.impl;
 
+import java.util.ArrayList;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.ShoppingCart.dao.CustomerDao;
+import com.ShoppingCart.entity.Customer;
 
 public class CustomerDaoImpl implements CustomerDao {
+
+	
+	@SuppressWarnings("unused")
+	private final Log logger = LogFactory.getLog(getClass());
+	
+	private SessionFactory sessionFactory;
+	@SuppressWarnings("unused")
+	private Session session;
+	
+	@Autowired
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public Session getSession() {
+	  return sessionFactory.getCurrentSession();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	@Transactional
+	public ArrayList<Customer> getAllCustomers() {
+		return (ArrayList<Customer>) getSession().createCriteria(Customer.class).list();
+	}
+
+	@Override
+	@Transactional
+	public Customer getCustomer(int id) {
+		return (Customer) getSession().createCriteria(Customer.class).add(Restrictions.eq("id",id)).uniqueResult();
+	}
+
+	@Override
+	@Transactional
+	public void addCustomer(Customer customer) {
+		getSession().save(customer);
+		getSession().getTransaction().commit();
+	}
+
+	@Override
+	@Transactional
+	public void editCustomer(int id, Customer newCustomer) {
+		newCustomer.setId(id);
+		getSession().saveOrUpdate(newCustomer);
+		getSession().getTransaction().commit();
+	}
+
+	@Override
+	@Transactional
+	public void deleteCustomer(int id) {
+		getSession().delete(id);
+		getSession().getTransaction().commit();
+	}
 
 }
